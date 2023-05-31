5129b8a8bff3dd7a288fa283348889f5ed86e48d
BeeMOre32 <windowssart01@gmail.com>
2023-05-31 15:16:53 +0900

    refact: 컴포넌트 분리화 및 추상화

--- src/components/main/MainResellerGraph.tsx
+++ src/components/main/MainResellerGraph.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import * as S from '@/styles/Main.styled';
 import { mainGraphData } from '@/constants/mainGraph';
+import { BsArrowUpRight } from 'react-icons/bs';
 
 function MainResellerGraph() {
   return (
-    <S.MainReSellerTable>
-      {mainGraphData.map((mainGraphDatum) => (
-        <S.MainReSellerTableContainer key={mainGraphDatum.graphValue}>
-          <p className="paragraph">{mainGraphDatum.title}</p>
-          <div className="graph">
-            <S.MainReSellerTableGraph graphSize={mainGraphDatum.graphSize} />
-            <p>{mainGraphDatum.graphValue}</p>
-          </div>
-        </S.MainReSellerTableContainer>
-      ))}
-    </S.MainReSellerTable>
+    <S.MainReSellerContent>
+      <h1>
+        판매액이 높을수록 <br /> 수수료가 낮아집니다.
+        <BsArrowUpRight />
+      </h1>
+      <p>판매 금액대별 대행 수수료</p>
+      <S.MainReSellerTable>
+        {mainGraphData.map((mainGraphDatum) => (
+          <S.MainReSellerTableContainer key={mainGraphDatum.graphValue}>
+            <p className="paragraph">{mainGraphDatum.title}</p>
+            <div className="graph">
+              <S.MainReSellerTableGraph graphsize={mainGraphDatum.graphSize} />
+              <p>{mainGraphDatum.graphValue}</p>
+            </div>
+          </S.MainReSellerTableContainer>
+        ))}
+      </S.MainReSellerTable>
+    </S.MainReSellerContent>
   );
 }
 
