004846433215f07729cd1f2737055189642ce2fa
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-05 16:17:28 +0900

    feat: 기본적으로 첫 FAQ는 오픈 상태로 설정

--- src/components/common/FAQ/FAQItem/index.tsx
+++ src/components/common/FAQ/FAQItem/index.tsx
@@ -1,22 +1,27 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { replaceStringsWithTags } from '@/utils';
 
 import { FiArrowUpLeft, FiArrowDownRight } from 'react-icons/fi';
-import * as S from './style';
 import { faqType } from '@/types/constants/faqType';
+
+import * as S from './style';
 
 interface FAQItemProps {
   faq: faqType;
+  index: number;
 }
 
-function FAQItem({ faq }: FAQItemProps) {
-  const [isFAQOpen, setIsFAQOpen] = useState(false);
+function FAQItem({ faq, index }: FAQItemProps) {
+  const [isFAQOpen, setIsFAQOpen] = useState(index === 0 ? true : false);
+  const [height, setHeight] = useState<string>('');
   const descRef = useRef<HTMLDivElement | null>(null);
   const { title, description, info, category } = faq;
 
-  const cardContentStyle = {
-    height: isFAQOpen && descRef.current ? `${descRef.current.getBoundingClientRect().height + 16}px` : '0px',
-  };
+  useEffect(() => {
+    if (descRef.current !== null) {
+      setHeight(isFAQOpen ? `${descRef.current.getBoundingClientRect().height + 16}px` : '0px');
+    }
+  }, [descRef, isFAQOpen]);
 
   return (
     <S.FAQItem onClick={() => setIsFAQOpen(!isFAQOpen)} isFAQOpen={isFAQOpen}>
@@ -28,7 +33,7 @@ function FAQItem({ faq }: FAQItemProps) {
           <h3>{title}</h3>
         </S.CardHeader>
 
-        <S.CardContent isFAQOpen={isFAQOpen} style={cardContentStyle}>
+        <S.CardContent isFAQOpen={isFAQOpen} style={{ height }}>
           <hr />
           {<p dangerouslySetInnerHTML={replaceStringsWithTags(description)} ref={descRef}></p>}
         </S.CardContent>
