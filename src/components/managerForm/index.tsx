2d376c67f73d888f2ea709b365d6bbf8b85507a1
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-07 20:17:59 +0900

    design: FieldExperienceForm 컴포넌트 구현

--- src/components/managerForm/index.tsx
+++ src/components/managerForm/index.tsx
@@ -98,6 +98,25 @@ function ManagerForm() {
             </li>
           </ul>
         </S.CertificateForm>
+
+        <S.FieldExperienceForm>
+          <S.SubTitle>
+            정리수납 관련 현장 경험이 있으시다면
+            <br />
+            상세하게 입력해주세요.
+          </S.SubTitle>
+
+          <p>현장에서 옷장정리를 한 경험을 위주로 입력해주세요.</p>
+
+          <textarea
+            name=""
+            id=""
+            placeholder="현장 파견 20회 이상, 옷장 정리 경험 10회 이상"
+            style={{ resize: 'none' }}
+          ></textarea>
+
+          <span>0 / 150</span>
+        </S.FieldExperienceForm>
       </div>
     </S.ManagerFormContainer>
   );
