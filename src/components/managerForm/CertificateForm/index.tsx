f6172d5ecf457575b4764c77557dd796cb436c9d
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-08 02:20:52 +0900

    feat: 자격증 선택하기 버튼, 옵션 버튼 기능 구현
    * 토글
    * 선택한 옵션 state 관리 및 버튼 텍스트로 변경

--- src/components/managerForm/CertificateForm/index.tsx
+++ src/components/managerForm/CertificateForm/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { BsChevronDown } from 'react-icons/bs';
 import * as G from '../style';
 import * as S from './style';
 
 function CertificateForm() {
+  const [isOptionsOpen, setIsOptionsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('자격증 선택하기');
+
+  const selectOptionHandler = (option: string) => {
+    setSelectedOption(option);
+  };
+
   return (
     <S.CertificateForm>
       <G.SubTitle>
@@ -14,35 +21,86 @@ function CertificateForm() {
 
       <span>(1개만 선택가능)</span>
 
-      <S.SelectButton type="button">
-        자격증 선택하기
+      <S.SelectButton type="button" onClick={() => setIsOptionsOpen(!isOptionsOpen)}>
+        {selectedOption}
         <BsChevronDown />
       </S.SelectButton>
-      <ul>
-        <li>
-          <S.OptionButton type="button">1급 (오프라인 취득)</S.OptionButton>
-        </li>
 
-        <li>
-          <S.OptionButton type="button">2급 (오프라인 취득)</S.OptionButton>
-        </li>
+      {isOptionsOpen && (
+        <ul>
+          <li>
+            <S.OptionButton
+              type="button"
+              onClick={() => {
+                setIsOptionsOpen(false);
+                selectOptionHandler('1급 (오프라인 취득');
+              }}
+            >
+              1급 (오프라인 취득)
+            </S.OptionButton>
+          </li>
 
-        <li>
-          <S.OptionButton type="button">1급 (온라인 취득)</S.OptionButton>
-        </li>
+          <li>
+            <S.OptionButton
+              type="button"
+              onClick={() => {
+                setIsOptionsOpen(false);
+                selectOptionHandler('2급 (오프라인 취득');
+              }}
+            >
+              2급 (오프라인 취득)
+            </S.OptionButton>
+          </li>
 
-        <li>
-          <S.OptionButton type="button">2급 (온라인 취득)</S.OptionButton>
-        </li>
+          <li>
+            <S.OptionButton
+              type="button"
+              onClick={() => {
+                setIsOptionsOpen(false);
+                selectOptionHandler('1급 (온라인 취득');
+              }}
+            >
+              1급 (온라인 취득)
+            </S.OptionButton>
+          </li>
 
-        <li>
-          <S.OptionButton type="button">없음</S.OptionButton>
-        </li>
+          <li>
+            <S.OptionButton
+              type="button"
+              onClick={() => {
+                setIsOptionsOpen(false);
+                selectOptionHandler('2급 (온라인 취득');
+              }}
+            >
+              2급 (온라인 취득)
+            </S.OptionButton>
+          </li>
 
-        <li>
-          <S.OptionButton type="button">기타</S.OptionButton>
-        </li>
-      </ul>
+          <li>
+            <S.OptionButton
+              type="button"
+              onClick={() => {
+                setIsOptionsOpen(false);
+                selectOptionHandler('없음');
+              }}
+            >
+              없음
+            </S.OptionButton>
+          </li>
+
+          <li>
+            <S.OptionButton
+              type="button"
+              onClick={(e) => {
+                setIsOptionsOpen(false);
+                selectOptionHandler('기타');
+              }}
+            >
+              기타
+            </S.OptionButton>
+          </li>
+        </ul>
+      )}
     </S.CertificateForm>
   );
 }
