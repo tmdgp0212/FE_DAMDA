0c1ad98993c634d7afd49c484f1c89c8d456416d
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-09 19:28:13 +0900

    refactor: react-hook-form에서 useReducer로 변경
    * Introduction 컴포넌트

--- src/components/managerForm/introductionForm/index.tsx
+++ src/components/managerForm/introductionForm/index.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import * as S from './style';
 
 interface IntroductionFormProps {
-  info: string;
-  placeholder: string;
-  register: any;
+  dispatch: any;
 }
 
-function IntroductionForm({ info, placeholder, register }: IntroductionFormProps) {
+function IntroductionForm({ dispatch }: IntroductionFormProps) {
+  const nameChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: 'NAME', payload: { name: e.target.value } });
+  };
+
+  const phoneNumberChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: 'PHONE_NUMBER', payload: { phoneNumber: e.target.value } });
+  };
+
   return (
-    <S.FormInput>
-      <span>{info}</span>
-      <input type="text" placeholder={placeholder} {...register(info)} />
-    </S.FormInput>
+    <>
+      <S.FormInput>
+        <span>이름</span>
+        <input type="text" placeholder="김열다" onChange={nameChangeHandler} />
+      </S.FormInput>
+
+      <S.FormInput>
+        <span>연락처</span>
+        <input type="text" placeholder="010-0000-0000" onChange={phoneNumberChangeHandler} />
+      </S.FormInput>
+    </>
   );
 }
 
