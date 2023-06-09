8561259f3522dd93b6bdd6d6e45a040c4cc37b77
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-06-09 19:42:54 +0900

    refactor: react-hook-form에서 useReducer로 변경
    * DaySelectionForm 컴포넌트

--- src/components/managerForm/DaySelectionForm/index.tsx
+++ src/components/managerForm/DaySelectionForm/index.tsx
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 import * as G from '../style';
 import * as S from './style';
 
-function DaySelectionForm({ register }: { register: any }) {
+function DaySelectionForm({ dispatch }: any) {
+  const dayCheckHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: 'DAY', payload: { day: e.target.value } });
+  };
+
   return (
     <S.DaySelectionForm>
       <G.SubTitle>활동 가능한 요일을 선택해주세요.</G.SubTitle>
 
       <div>
-        <input type="checkbox" name="day" id="mon" value="mon" {...register('checkbox_field')} />
+        <input type="checkbox" name="day" id="mon" value="mon" onChange={dayCheckHandler} />
         <label htmlFor="mon">월</label>
 
-        <input type="checkbox" name="day" id="tue" value="tue" {...register('checkbox_field')} />
+        <input type="checkbox" name="day" id="tue" value="tue" onChange={dayCheckHandler} />
         <label htmlFor="tue">화</label>
 
-        <input type="checkbox" name="day" id="wed" value="wed" {...register('checkbox_field')} />
+        <input type="checkbox" name="day" id="wed" value="wed" onChange={dayCheckHandler} />
         <label htmlFor="wed">수</label>
 
-        <input type="checkbox" name="day" id="thu" value="thu" {...register('checkbox_field')} />
+        <input type="checkbox" name="day" id="thu" value="thu" onChange={dayCheckHandler} />
         <label htmlFor="thu">목</label>
 
-        <input type="checkbox" name="day" id="fri" value="fri" {...register('checkbox_field')} />
+        <input type="checkbox" name="day" id="fri" value="fri" onChange={dayCheckHandler} />
         <label htmlFor="fri">금</label>
 
-        <input type="checkbox" name="day" id="sat" value="sat" {...register('checkbox_field')} />
+        <input type="checkbox" name="day" id="sat" value="sat" onChange={dayCheckHandler} />
         <label htmlFor="sat">토</label>
 
-        <input type="checkbox" name="day" id="sun" value="sun" {...register('checkbox_field')} />
+        <input type="checkbox" name="day" id="sun" value="sun" onChange={dayCheckHandler} />
         <label htmlFor="sun">일</label>
       </div>
     </S.DaySelectionForm>
