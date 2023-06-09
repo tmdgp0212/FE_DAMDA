3954383ff20e80e203b5e241f1f7a226c0d50519
BeeMOre32 <windowssart01@gmail.com>
2023-06-09 14:15:54 +0900

    fix: git으로 누락된 코드 최신화

--- src/components/common/Carousel/index.tsx
+++ src/components/common/Carousel/index.tsx
@@ -33,4 +33,5 @@ function Index({ carouselItems }: CarouselProps) {
   );
 }
 
+//git test
 export default Index;
--- src/components/common/Carousel/style.ts
+++ src/components/common/Carousel/style.ts
@@ -61,3 +61,5 @@ export const UserReviewWrapper = styled.div<{
     }
   }
 `;
+
+// git test
