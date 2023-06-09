a208e9ee85086dacf7fa054f382573b4c58aa79e
BeeMOre32 <windowssart01@gmail.com>
2023-06-09 20:39:51 +0900

    feat: firstStep 말고 secStep을 가질 수 있도록 구조 변경

--- src/components/usersurvey/components/Input.tsx
+++ src/components/usersurvey/components/Input.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import { UserSurveyFormInputWrapper } from '@/styles/survey.styled';
 import { UserSurveyFormTitleProps } from '@/types/components/form';
-import { convertQuestionIdentifierToKorean, convertQuestionIdentifierToPlaceholder } from '@/utils';
+import {
+  convertQuestionIdentifierToKorean,
+  convertQuestionIdentifierToPlaceholder,
+  replaceStringsWithTags,
+} from '@/utils';
+import { UserSurveyForm } from '@/store/userSurvey';
 
-function Input({ title, placeholder }: UserSurveyFormTitleProps) {
+function Input({ title, placeholder, questionNumber, handleUpdateFormValue }: UserSurveyFormTitleProps) {
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const currentData: UserSurveyForm = {
+      questionNumber,
+      answer: value,
+      questionIdentifier: placeholder,
+    };
+
+    handleUpdateFormValue((prev) => {
+      const isExist = prev.find((data) => data.questionNumber === questionNumber);
+      if (isExist) {
+        return prev.map((data) => (data.questionNumber === questionNumber ? currentData : data));
+      } else {
+        return [...prev, currentData];
+      }
+    });
+  };
+
   return (
     <UserSurveyFormInputWrapper hasspan={!!title}>
-      {title && <span>{title}</span>}
+      {title && <span dangerouslySetInnerHTML={replaceStringsWithTags(title)}></span>}
       <div className="input">
         {placeholder && <p>{convertQuestionIdentifierToKorean(placeholder)}</p>}
-        <input type="text" placeholder={convertQuestionIdentifierToPlaceholder(placeholder)} />
+        <input
+          type="text"
+          placeholder={convertQuestionIdentifierToPlaceholder(placeholder)}
+          onChange={onChangeHandler}
+        />
       </div>
     </UserSurveyFormInputWrapper>
   );
