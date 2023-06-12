994dc82dca98f085fb4463ad4ac07070043e9a29
BeeMOre32 <windowssart01@gmail.com>
2023-06-12 15:30:24 +0900

    feat: location select 를 위한 상수 선언

---
+++ src/constants/location.ts
@@ -0,0 +1,17 @@
+import { AddressList } from '@/types/constants/userSurvey';
+
+export const addressList: AddressList = {
+  서울특별시: [
+    '전체',
+    '강남구',
+    '강동구',
+    '강북구',
+    '강서구',
+    '관악구',
+    '구로구',
+    '금천구',
+    '은평구',
+    '더 많은 지역이 추가 될 예정입니다!',
+  ],
+  경기도: ['기흥구', '분당구', '연천군', '남양주시', '화성시'],
+};
