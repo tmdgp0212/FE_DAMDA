3b123568cfa17d38549a1b9a1fdb4e04eaafb4f7
BeeMOre32 <windowssart01@gmail.com>
2023-06-22 21:05:46 +0900

    feat: 인원수와 서비스 시간을 선택하면 둘다 price 설정

--- src/components/usersurvey/step/FirstStep.tsx
+++ src/components/usersurvey/step/FirstStep.tsx
@@ -3,6 +3,7 @@ import { UserSurveyFormDataType } from '@/types/api/formTypes';
 import FormElements from '@/components/usersurvey/FormElements';
 import { UserSurveyForm, useUserSurveyForm } from '@/store/userSurvey';
 import { UserSurveyFormNextBox } from '@/styles/survey.styled';
+import { getTotalPrice } from '@/utils';
 
 interface FirstStepProps {
   handleNextStep: () => void;
@@ -12,7 +13,7 @@ function FirstStep({ handleNextStep, userSurveyFormData }: FirstStepProps) {
   const [formValue, setFormValue] = useState<UserSurveyForm[]>([]);
   const [isValid, setIsValid] = useState<boolean>(false);
 
-  const { setUserSurveyForm, userSurveyForm } = useUserSurveyForm();
+  const { setUserSurveyForm, userSurveyForm, setPrice, setPerPerson } = useUserSurveyForm();
   const checkRequiredQuestions = (formValue: UserSurveyForm[]) => {
     const requiredQuestionsIndex = userSurveyFormData
       .filter((data) => data.required)
@@ -25,8 +26,26 @@ function FirstStep({ handleNextStep, userSurveyFormData }: FirstStepProps) {
     setIsValid(isAllRequiredQuestionsAnswered);
   };
 
+  const checkPersonNTime = (formValue: UserSurveyForm[]) => {
+    const person = formValue.find((data) => data.questionIdentify === 'AFEWSERVINGS')?.answer;
+    const time = formValue.find((data) => data.questionIdentify === 'SERVICEDURATION')?.answer;
+
+    if (!person || !time) return;
+
+    const personNum = person.replace(/\D/g, '');
+    const timeNum = time.replace(/\D/g, '');
+    if (!personNum || !timeNum) return;
+    if (personNum === '' || timeNum === '') return;
+    if (personNum === '0' || timeNum === '0') return;
+
+    const price = getTotalPrice(Number(timeNum), Number(personNum));
+    setPrice(price.price);
+    setPerPerson(price.perPerson);
+  };
+
   useEffect(() => {
     checkRequiredQuestions(formValue);
+    checkPersonNTime(formValue);
   }, [formValue]);
 
   useEffect(() => {
@@ -35,7 +54,6 @@ function FirstStep({ handleNextStep, userSurveyFormData }: FirstStepProps) {
     }
   }, []);
 
-  console.log(isValid);
   const handleGoNextStep = () => {
     setUserSurveyForm(formValue);
     handleNextStep();
