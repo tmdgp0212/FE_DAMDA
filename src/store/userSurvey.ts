ca142bb018d3d102debaf4f27adc5942cffe32fa
BeeMOre32 <windowssart01@gmail.com>
2023-06-22 18:50:06 +0900

    feat: price 를 store에 저장함

--- src/store/userSurvey.ts
+++ src/store/userSurvey.ts
@@ -8,15 +8,19 @@ export type UserSurveyForm = {
 
 export type UserSurveyStore = {
   userSurveyForm: UserSurveyForm[];
+  price: number;
 };
 
 type UserSurveyStoreActions = {
   setUserSurveyForm: (userSurveyForm: UserSurveyForm[]) => void;
+  setPrice: (price: number) => void;
 };
 
 const useUserSurveyStore = create<UserSurveyStore & UserSurveyStoreActions>((set) => ({
   userSurveyForm: [],
+  price: 0,
   setUserSurveyForm: (userSurveyForm) => set({ userSurveyForm }),
+  setPrice: (price) => set({ price }),
 }));
 
 export const useUserSurveyForm = () => useUserSurveyStore((state) => state);
