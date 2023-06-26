cde33593b76fb1d8f222814976659b942d033675
BeeMOre32 <windowssart01@gmail.com>
2023-06-26 21:15:28 +0900

    fix: 사용하지 않았던 import 최적화

--- src/pages/manager/accept/[id].tsx
+++ src/pages/manager/accept/[id].tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import AcceptLayout from '@/components/manager/accept/AcceptLayout';
 import { GetServerSideProps } from 'next';
 import { MatchingData, MatchingManagerData } from '@/apis/manager';
-import axios from 'axios';
 
 export const getServerSideProps: GetServerSideProps<{ data: MatchingData }> = async ({ params }) => {
   const res = await fetch(`https://api.damda.store/api/v1/matching/accept/${params?.id}?memberId=5`);
