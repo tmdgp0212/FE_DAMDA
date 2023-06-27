174e8d03ef2023d79a9c8349e0c60d7b33d3938b
BeeMOre32 <windowssart01@gmail.com>
2023-06-27 20:08:49 +0900

    fix: store의 값을 null 로 수정하여 false 에 라우트가 트리거 되도록 변경

--- src/pages/usersurvey/index.tsx
+++ src/pages/usersurvey/index.tsx
@@ -2,24 +2,20 @@ import React, { useEffect } from 'react';
 import UserSurvey from '@/components/usersurvey';
 import useAuthStore from '@/store/auth';
 import { useRouter } from 'next/router';
-import { UserLoginNeed } from '@/styles/survey.styled';
 
 function Index() {
   const router = useRouter();
 
   const { user } = useAuthStore();
 
-  return (
-    <>
-      {user.isLogin && <UserSurvey />}
-      {!user.isLogin && (
-        <UserLoginNeed>
-          <h1>설문을 진행할려면 로그인이 필요합니다.</h1>
-          <button onClick={() => router.push('/login')}>이 버튼을 눌러서 로그인 화면으로 이동해주세요!</button>
-        </UserLoginNeed>
-      )}
-    </>
-  );
+  useEffect(() => {
+    if (user === null) return;
+    if (user.isLogin === false) {
+      router.push('/login');
+    }
+  }, [user]);
+
+  return <UserSurvey />;
 }
 
 export default Index;
