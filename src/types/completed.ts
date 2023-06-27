8ff0c6347ef7d1f587b016007c575f79d65c309e
Nevacat <nevacattery217@naver.com>
2023-06-27 13:29:39 +0900

    feat: 매니저단, 유저단 기능 구현 및 수정
    링크연결
    매니저 서비스 완료 폼 추가

---
+++ src/types/completed.ts
@@ -0,0 +1,9 @@
+export interface ReservationData {
+  data:any;
+  managerNames: string;
+  serviceDate: string;
+  serviceAddress: string;
+  managerCount: number;
+  serviceUsageTime: string;
+  reservationId: number;
+}
