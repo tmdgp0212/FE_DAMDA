a5ce9789d015b9d1dadcdfdb893719a9bd6517f1
BeeMOre32 <windowssart01@gmail.com>
2023-06-28 17:57:00 +0900

    fix: 매니저 매칭 로직 수정

--- src/components/manager/accept/AcceptLayout.tsx
+++ src/components/manager/accept/AcceptLayout.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
-import { managerAcceptanceType } from '@/types/constants/manager';
-import { useMutation } from '@tanstack/react-query';
-import { postManagerAccept } from '@/apis/manager';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { getMatchingData, postManagerAccept } from '@/apis/manager';
 import { useRouter } from 'next/router';
-import YesPage from '@/components/manager/accept/YesPage';
 import Matching from '@/components/manager/accept/Matching';
+import YesPage from '@/components/manager/accept/YesPage';
 import NoPage from '@/components/manager/accept/NoPage';
 import Error from '@/components/manager/accept/Error';
 
-function AcceptLayout({ data }: { data: managerAcceptanceType }) {
+function AcceptLayout() {
   const [isAccept, setIsAccept] = useState<boolean | null>(null);
   const [isError, setIsError] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
 
   const router = useRouter();
   const path = router.query.id;
+
+  const { data } = useQuery(['managerAccept', path], () => getMatchingData(Number(path)));
 
   const { mutate } = useMutation(postManagerAccept);
 
@@ -33,6 +34,7 @@ function AcceptLayout({ data }: { data: managerAcceptanceType }) {
     );
   };
 
+  if (!data) return <div>loading...</div>;
   if (isAccept === null)
     return (
       <Matching data={data} handleAccept={handleAccept}>
