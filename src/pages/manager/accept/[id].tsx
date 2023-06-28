7ea8a254e8b12cb79c82edc8939fea4ce31e0b49
BeeMOre32 <windowssart01@gmail.com>
2023-06-28 17:57:22 +0900

    clean: 사용하지 않는 로직 수정

--- src/pages/manager/accept/[id].tsx
+++ src/pages/manager/accept/[id].tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import AcceptLayout from '@/components/manager/accept/AcceptLayout';
 
-import { managerAcceptanceType } from '@/types/constants/manager';
-
 function Id() {
   return <AcceptLayout />;
 }
