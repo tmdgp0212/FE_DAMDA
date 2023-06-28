d9c0e031fcfb90ded669201a76da30a2b3337068
seunghye <tmdgp0212@naver.com>
2023-06-28 13:48:46 +0900

    feat: kakao로그인 rest api key 변경

--- src/hook/useAuth.ts
+++ src/hook/useAuth.ts
@@ -9,7 +9,6 @@ export const useAuth = () => {
       if (data.data.profileImage === '404.jpg') {
         data.data.profileImage = 'http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_110x110.jpg';
       }
-      console.log(data.data);
       setUser(data.data);
     },
     onError: () => {
