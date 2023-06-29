342393ce27e781cbf1eabac6ee9a0c486a4a6d5f
BeeMOre32 <windowssart01@gmail.com>
2023-06-29 18:34:14 +0900

    fix: 메인 페이지에서 리뷰가 보이지 않던 문제 해결

--- src/components/main/UserReview.tsx
+++ src/components/main/UserReview.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from '@/styles/Main.styled';
 import Carousel from '@/components/common/Carousel';
 import { useQuery } from '@tanstack/react-query';
 import { getReviews } from '@/apis/review';
+import { ReviewData } from '@/types/review';
 
 function UserReview() {
   const { data: review } = useQuery(['review'], getReviews);
+  const [reviewData, setReviewData] = useState<ReviewData[]>();
 
-  const bestReview = review?.data.filter((item) => item.bestReview)[0];
-  const firstItem = review?.data.filter((item) => !item.bestReview)[0];
+  useEffect(() => {
+    if (!review) return;
+    console.log(review.data);
 
-  if (!bestReview || !firstItem) return null;
+    const bestReview = review?.data.filter((item) => item.bestReview)[0];
+    const firstItem = review?.data.filter((item) => !item.bestReview)[0];
 
-  const combinedArray = [bestReview, firstItem];
+    setReviewData([bestReview, firstItem]);
+  }, [review]);
 
   return (
     <>
-      {review && <Carousel carouselItems={combinedArray} height={32} width={100} />}
+      {reviewData && <Carousel carouselItems={reviewData} height={32} width={100} />}
       <S.MainMoreReviewButton href={'/review'}>더 많은 후기 보러가기</S.MainMoreReviewButton>
     </>
   );
